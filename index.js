f6be9b8aa68809b6b1911dea812abfe917fac79d
Anaclara <ana.c.jesus6@edu.senai.br>
2026-08-06 14:22:54 -0300

    feat:adiciona um array de produtos

---
+++ index.js
@@ -0,0 +1,22 @@
+let produtos = [
+    {
+        nome: 'Cola coca',
+        preco: 19,
+    },
+    {
+        nome: 'Fanta',
+        preco: 15,
+    },
+    {
+        nome: 'Guaraná',
+        preco: 17,
+    },
+    {
+        nome: 'Pepsi',
+        preco: 18,
+    },
+];
+
+console.log("lista de produtos:");
+console.log("===========================");
+console.table(produtos);
